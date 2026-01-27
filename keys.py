9b963acf8bf443884c333cedde3cde1acc3da995
Michael <michael3437@outlook.com>
2026-01-27 00:37:41 +0000

    Add initial handling of keys.

---
+++ keys.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from cryptography.hazmat.primitives import serialization
+from clients import KalshiHttpClient, KalshiWebSocketClient
+
+load_dotenv()
+KEYID = os.environ.get("KEYID")
+try:
+    with open("apikey.txt", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None
+        )
+except FileNotFoundError:
+    raise FileNotFoundError(f"Private key file not found at {KEYFILE}")
+except Exception as e:
+    raise Exception(f"Error loading private key: {str(e)}")
+
+def get_keys():
+    """Returns (KEYID, private_key)."""
+    return (KEYID, private_key)
